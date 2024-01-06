7b9c41bb5a018c723182a8c3384a89492bd2858e
James Forwood <james.jcs.forwood@gmail.com>
2024-01-06 22:50:08 +0000

    Update elegoo_install.sh

--- elegoo_install.sh
+++ elegoo_install.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+
+# elegoo_install.sh by James Forwood (uberoptix)
+# Designed to install the Elegoo 3.5" RPi display per https://github.com/MrYacha/LCD-show
+# Last updated 6 Jan 2024
+
 cd ~/
 sudo rm -rf LCD-show
 git clone https://github.com/goodtft/LCD-show.git
